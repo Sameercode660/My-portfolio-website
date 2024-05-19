b0ba09e9e4347a39f99ba978b533d6dc296b3462
Mohd Sameer <mesh789736@gmail.com>
2024-05-19 14:01:49 +0530

    modify app.jsx;

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,16 @@ import { Link, Outlet } from "react-router-dom";
 
 function App() {
 
-  const [position, setPosition] = useState('-70%')
+  const [position, setPosition] = useState(false)
 
   function handleOpenCloseNavigation() {
-    if(position.includes('-')) {
-      setPosition('0')
-    } else {
-      setPosition('-70%')
-    }
+    setPosition(prev => !prev)
   }
   return (
     <>
-      <div className="w-full flex justify-center items-center max-sm:justify-around border-2 max-sm:border-red-500">
+      <div className="w-full flex justify-center items-center max-sm:justify-around">
         <div className="flex w-[80%] justify-around m-2 p-1 max-sm:space-x-12 max-sm:w-full">
-          <div className="max-sm:border-2 max-sm:border-red-500">
+          <div className="">
             <h1
               className="text-3xl font-bold max-sm:text-[1.6rem]"
               style={{ fontFamily: "sans-serif" }}
@@ -43,9 +39,6 @@ function App() {
             >
               <span>Projects</span>
             </Link>
-            {/* <Link className="text-lg font-bold text-gray-300 hover:text-sky-400" to={"/About"}>
-            <span>About</span>
-            </Link> */}
             <Link
               className="text-lg font-bold text-gray-300 hover:text-sky-400"
               to={"/contact"}
@@ -53,7 +46,7 @@ function App() {
               <span>Contact Me</span>
             </Link>
           </div>
-          <div className="sm:hidden max-sm:grid place-items-center max-sm:border-2 max-sm:border-red-500 w-[2.5rem] " onClick={handleOpenCloseNavigation}>
+          <div className="sm:hidden max-sm:grid place-items-center w-[2.5rem] " onClick={handleOpenCloseNavigation}>
             <i class="fa-solid fa-bars"></i>
           </div>
         </div>
@@ -63,29 +56,29 @@ function App() {
       </div>
 
       <div
-        className={`border-2 border-red-500 w-[70%] h-[100vh] fixed z-10 top-0 left-[${position}] right-0 bottom-0 flex flex-col items-center gap-3 transition-all ease-in duration-500`}
+        className={` sm:hidden w-[70%] h-[100vh] fixed z-10 top-0 ${position ? "left-0" : "left-[-70%]"} right-0 bottom-0 flex flex-col items-center gap-3 transition-all ease-in duration-500`}
         style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
       >
         <Link
-          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full border-2 border-red-500 p-2 grid place-items-center m-1"
+          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full p-2 grid place-items-center m-1"
           to={"/"}
         >
           <span>Home</span>
         </Link>
         <Link
-          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full border-2 border-red-500 p-2 grid place-items-center m-1"
+          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full p-2 grid place-items-center m-1"
           to={"/skills"}
         >
           <span>Skills</span>
         </Link>
         <Link
-          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full border-2 border-red-500 p-2 grid place-items-center m-1"
+          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full  p-2 grid place-items-center m-1"
           to={"/Projects"}
         >
           <span>Projects</span>
         </Link>
         <Link
-          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full border-2 border-red-500 p-2 grid place-items-center m-1"
+          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full p-2 grid place-items-center m-1"
           to={"/contact"}
         >
           <span>Contact Me</span>
