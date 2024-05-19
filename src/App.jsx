7f81a84aa7e0728dfcd87a0b48f0a26e69dafd79
Mohd Sameer <mesh789736@gmail.com>
2024-05-19 14:22:08 +0530

    modify app.jsx ;

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 function App() {
-
-  const [position, setPosition] = useState(false)
+  const [position, setPosition] = useState(false);
 
   function handleOpenCloseNavigation() {
-    setPosition(prev => !prev)
+    setPosition((prev) => !prev);
   }
   return (
     <>
@@ -46,7 +45,10 @@ function App() {
               <span>Contact Me</span>
             </Link>
           </div>
-          <div className="sm:hidden max-sm:grid place-items-center w-[2.5rem] " onClick={handleOpenCloseNavigation}>
+          <div
+            className="sm:hidden max-sm:grid place-items-center w-[2.5rem] "
+            onClick={handleOpenCloseNavigation}
+          >
             <i class="fa-solid fa-bars"></i>
           </div>
         </div>
@@ -56,36 +58,43 @@ function App() {
       </div>
 
       <div
-        className={` sm:hidden w-[70%] h-[100vh] fixed z-10 top-0 ${position ? "left-0" : "left-[-70%]"} right-0 bottom-0 flex flex-col items-center gap-3 transition-all ease-in duration-500`}
+        className={` sm:hidden w-[70%] h-[100vh] fixed z-100 top-0 ${
+          position ? "left-0" : "left-[-70%]"
+        } right-0 bottom-0 flex flex-col items-center gap-3 transition-all ease-in duration-500`}
         style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
       >
-        <Link
-          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full p-2 grid place-items-center m-1"
-          to={"/"}
-        >
-          <span>Home</span>
-        </Link>
-        <Link
-          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full p-2 grid place-items-center m-1"
-          to={"/skills"}
-        >
-          <span>Skills</span>
-        </Link>
-        <Link
-          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full  p-2 grid place-items-center m-1"
-          to={"/Projects"}
-        >
-          <span>Projects</span>
-        </Link>
-        <Link
-          className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full p-2 grid place-items-center m-1"
-          to={"/contact"}
-        >
-          <span>Contact Me</span>
-        </Link>
-        <button className="grid place-items-center w-[40px] h-[40px] rounded-full bg-slate-500"  onClick={handleOpenCloseNavigation}>
-          <i class="fa-solid fa-xmark text-[25px]"></i>
-        </button>
+        <div className="w-full grid place-items-center mt-10">
+          <Link
+            className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full p-2 grid place-items-center m-1"
+            to={"/"}
+          >
+            <span>Home</span>
+          </Link>
+          <Link
+            className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full p-2 grid place-items-center m-1"
+            to={"/skills"}
+          >
+            <span>Skills</span>
+          </Link>
+          <Link
+            className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full  p-2 grid place-items-center m-1"
+            to={"/Projects"}
+          >
+            <span>Projects</span>
+          </Link>
+          <Link
+            className="text-lg font-bold text-gray-300 hover:text-sky-400 w-full p-2 grid place-items-center m-1"
+            to={"/contact"}
+          >
+            <span>Contact Me</span>
+          </Link>
+          <button
+            className="grid place-items-center w-[40px] h-[40px] rounded-full bg-slate-500"
+            onClick={handleOpenCloseNavigation}
+          >
+            <i class="fa-solid fa-xmark text-[25px]"></i>
+          </button>
+        </div>
       </div>
     </>
   );
